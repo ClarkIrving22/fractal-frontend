fbd02c837faf67d07d271a1f30161723b6bc897c
ClarkIrving22 <clark.irving@icloud.com>
2023-05-04 19:24:33 -0500

    Creating View MyOrder and Table component

---
+++ src/views/MyOrders/ViewMyOrders.jsx
@@ -0,0 +1,11 @@
+import './styles.css'
+
+const ViewMyOrders = () => {
+    return(
+        <div className="ViewMyOrders-Container">
+            <div className="title">My Orders</div>
+        </div>
+    )
+}
+
+export default ViewMyOrders
